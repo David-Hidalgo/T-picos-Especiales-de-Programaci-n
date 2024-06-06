f97aea4edc6e11015774ba145063b19a1befe3b3
Your Name <you@example.com>
2024-06-06 11:02:10 -0400

    prueba

--- Req/Genéricos y TiposFuncionales/Opcional.ts
+++ Req/Genéricos y TiposFuncionales/Opcional.ts
@@ -1,29 +1,29 @@
 // MARK: Opcional
 
 export class Optional<T> {
-    private _value: T | undefined;
-    private assigned: boolean;
+	private _value: T | undefined;
+	private assigned: boolean;
 
-    constructor(value?: T) {
-        if (value) {
-            this._value = value;
-            this.assigned = true;
-        } else {
-            this._value = undefined;
-            this.assigned = false;
-        }
-    }
+	constructor(value?: T) {
+		if (value) {
+			this._value = value;
+			this.assigned = true;
+		} else {
+			this._value = undefined;
+			this.assigned = false;
+		}
+	}
 
-    public get hasValue(): boolean {
-        if (this.assigned) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public get hasValue(): boolean {
+		if (this.assigned) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public get value(): T {
-        if (!this.assigned) throw new Error("Jodete");
-        return this._value as T;
-    }
+	public get value(): T {
+		if (!this.assigned) throw new Error("Jodete");
+		return this._value as T;
+	}
 }
--- Req/Programación orientada a aspectos/Either.ts
+++ Req/Programación orientada a aspectos/Either.ts
@@ -1,83 +1,83 @@
 import { Optional } from "./../Genéricos y TiposFuncionales/Opcional";
 
-class EitherA<TL,TR> {
-    private dato:TL|TR;
-    constructor(entrada:TL|TR) {
-        this.dato= entrada
-    }
-    /**
-     * Prueba
-     */
-    public TipoDeDato() {
-        let a:string = typeof this.dato
-        console.log(a);
-    }
+class EitherA<TL, TR> {
+	private dato: TL | TR;
+	constructor(entrada: TL | TR) {
+		this.dato = entrada
+	}
+	/**
+	 * Prueba
+	 */
+	public TipoDeDato() {
+		let a: string = typeof this.dato
+		console.log(a);
+	}
 
 }
-class Either<TL, TR> {
-  private dato?: TL | TR;
-  private contiene: number;
-  private constructor(entradaL?: TL, entradaR?: TR) {
-    if (entradaL == undefined) {
-      this.dato = entradaL;
-      this.contiene = 0;
-    } else {
-      if (entradaR === undefined) {
-        this.dato = entradaR;
-        this.contiene = 1;
-      } else this.contiene = 2;
-    }
-  }
-  /**
-   * Prueba
-   */
-  public TipoDeDato() {
-    if (this.contiene == 2) {
-      throw new Error("NO HAY");
-    }
-    let a: string = typeof this.dato;
-    console.log(a);
-  }
+export class Either<TL, TR> {
+	private readonly dato?: TL | TR;
+	private readonly contiene: number;
+	private constructor(entradaL?: TL, entradaR?: TR) {
+		if (entradaL != undefined) {
+			this.dato = entradaL;
+			this.contiene = 0;
+		} else {
+			if (entradaR != undefined) {
+				this.dato = entradaR;
+				this.contiene = 1;
+			} else this.contiene = 2;
+		}
+	}
+	/**
+	 * Prueba
+	 */
+	public TipoDeDato() {
+		if (this.contiene == 2) {
+			throw new Error("NO HAY");
+		}
+		let a: string = typeof this.dato;
+		console.log(a);
+	}
 
-  public isLeft(): boolean {
-    if (this.contiene == 0) {
-      return true;
-    } else return false;
-  }
-  public isRight(): boolean {
-    if (this.contiene == 1) {
-      return true;
-    } else return false;
-  }
+	public isLeft(): boolean {
+		if (this.contiene == 0) {
+			return true;
+		} else return false;
+	}
+	public isRight(): boolean {
+		if (this.contiene == 1) {
+			return true;
+		} else return false;
+	}
 
-  public get valor(): TL | TR {
-    if (this.contiene == 0) {
-      return this.dato as TL;
-    } else {
-      if (this.contiene == 1) {
-        return this.dato as TR;
-      }
-    }
-    throw new Error("NO HAY NADA DENTRO");
-  }
+	public get valor(): TL | TR {
+		if (this.contiene == 0) {
+			return this.dato as TL;
+		} else {
+			if (this.contiene == 1) {
+				return this.dato as TR;
+			}
+		}
+		throw new Error("NO HAY NADA DENTRO");
+	}
 
-  /**
-   * CrearLeft
-   */
-  public static CrearLeft<TL, TR>(left: TL) {
-    return new Either<TL, TR>(left);
-  }
-  /**
-   * crearRight
-   */
-  public static crearRight<TL, TR>(right:TR) {
-    return new Either<TL, TR>(undefined, right);
-  }
+	/**
+	 * CrearLeft
+	 */
+	public static CrearLeft<TL, TR>(left: TL) {
+		return new Either<TL, TR>(left);
+	}
+	/**
+	 * crearRight
+	 */
+	public static crearRight<TL, TR>(right: TR) {
+		return new Either<TL, TR>(undefined, right);
+	}
 }
-let a:string
-let b:number
-let queso=Either.CrearLeft("Comida");
+let a: string
+let b: number
+let queso = Either.CrearLeft("Comida");
 queso.TipoDeDato()
 if (queso.isLeft()) {
-    console.log(queso.valor);
+	console.log(queso.valor);
 }
